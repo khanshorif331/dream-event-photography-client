46801a58d98a8dccc7f3980ab19c48a132b03513
khanshorif331 <khanshorif331@gmail.com>
2022-04-17 00:02:25 +0600

    added another section in the home components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg'
 import './App.css'
 import Header from './components/Shared/Header/Header'
+import Home from './components/Home/Home/Home'
 
 function App() {
 	return (
 		<div className='App'>
 			<Header></Header>
+			<Home></Home>
 		</div>
 	)
 }
---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './Home.css'
+import img1 from '../../../images/image1.jpeg'
+import img2 from '../../../images/image2.jpg'
+import img3 from '../../../images/image3.jpg'
+const Home = () => {
+	return (
+		<div>
+			<img src={img2} alt='' />
+		</div>
+	)
+}
+
+export default Home
