b46236108b721e833dc286630e1d483f470eaebb
khanshorif331 <khanshorif331@gmail.com>
2022-04-18 22:17:29 +0600

    checkout page info done

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Service from '../Home/Service/Service'
+import Services from '../Home/Services/Services'
 import './Checkout.css'
 
 const Checkout = () => {
 	return (
 		<div className='checkout'>
 			<h1>This is checkout page</h1>
+			<Services></Services>
 		</div>
 	)
 }
--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -4,11 +4,13 @@ import img1 from '../../../images/image1.jpeg'
 import img2 from '../../../images/image2.jpg'
 import img3 from '../../../images/image3.jpg'
 import Services from '../Services/Services'
+import Feedback from '../Feedback/Feedback'
 const Home = () => {
 	return (
 		<div>
 			<img src={img2} className='img-fluid mx-auto' alt='' />
 			<Services></Services>
+			<Feedback></Feedback>
 		</div>
 	)
 }
